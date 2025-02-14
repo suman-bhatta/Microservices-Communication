7ab456d2c81b3614129450cd9ce56be9b9815604
Suman Bhatta <sumanbhatta735@gmail.com>
2025-02-14 13:11:17 +0545

    Add API view to receive messages in consumer app

--- microservice/apps/consumer/views.py
+++ microservice/apps/consumer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+@api_view(['POST'])
+def receive_message(request):
+    data = request.data
+    print(f"Received message: {data}")  # Process the message
+    return Response({"status": "Message received", "data": data})
